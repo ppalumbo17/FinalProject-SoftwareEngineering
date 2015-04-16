3fe974b1b738cdfcc16be4c147e7ba747dbd1dbc
Peter Palumbo <ppalumbo@mines.edu>
2015-04-15 21:13:59 -0600

    Failing Tests First Draft

--- src/Final/GameRunner.java
+++ src/Final/GameRunner.java
@@ -5,4 +5,45 @@ public class GameRunner {
 	void calcTrajectory(){
 		
 	}
+
+	public void createCannon() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void createTarget(double x) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public double getAngle() {
+		return 0;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAngle(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setInitialVelocity(double i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public double getInitialVelocity() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getDistance() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+	public boolean targetReached() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
